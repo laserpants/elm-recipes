37a7af5969966476a16b22151a4a5d2edd342b1d
Heikki Johannes Hilden <hildenjohannes@gmail.com>
2020-02-20 22:17:22 +0300

    Build out application example

---
+++ examples/application/src/Form/Comment.elm
@@ -0,0 +1,101 @@
+module Form.Comment exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Json.Decode as Json
+import Json.Encode as Encode
+import Recipes.Form as Form exposing (FieldList, Validate, checkbox, inputField)
+import Recipes.Form.Validate as Validate
+
+
+type Field
+    = Email
+    | Body
+
+
+type alias Msg =
+    Form.Msg Field
+
+
+type alias Data =
+    { email : String
+    , body : String
+    }
+
+
+toJson : Data -> Json.Value
+toJson { email, body } =
+    Encode.object
+        [ ( "email", Encode.string email )
+        , ( "body", Encode.string body )
+        ]
+
+
+type alias Model =
+    Form.Model Field () Data
+
+
+validate : Validate Field () Data
+validate =
+    let
+        validateEmail =
+            Validate.stringNotEmpty ()
+
+        validateBody =
+            Validate.stringNotEmpty ()
+    in
+    Validate.record Data
+        |> Validate.inputField Email validateEmail
+        |> Validate.inputField Body validateBody
+
+
+init : FieldList Field () -> ( Model, Cmd Msg )
+init =
+    Form.init validate
+
+
+view : Model -> Html Msg
+view { fields, disabled } =
+    let
+        errorHelper field =
+            case Form.fieldError field of
+                Nothing ->
+                    text ""
+
+                Just error ->
+                    text "error"
+    in
+    Form.lookup2 fields
+        Email
+        Body
+        (\email body ->
+            [ fieldset
+                [ Html.Attributes.disabled disabled ]
+                [ div []
+                    [ label [] [ text "Email" ]
+                    ]
+                , div []
+                    [ input
+                        (Form.inputAttrs Email email)
+                        []
+                    , div [] [ errorHelper email ]
+                    ]
+                , div []
+                    [ label [] [ text "Body" ]
+                    ]
+                , div []
+                    [ textarea
+                        (Form.inputAttrs Body body)
+                        []
+                    , div [] [ errorHelper body ]
+                    ]
+                , div []
+                    [ button []
+                        [ text "Publish"
+                        ]
+                    ]
+                ]
+            ]
+                |> Html.form [ onSubmit Form.Submit ]
+        )
