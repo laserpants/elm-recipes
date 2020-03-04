edbafec6fe93f8ede9b3bfb499edeaafb795b67b
Heikki Johannes Hilden <hildenjohannes@gmail.com>
2020-03-05 00:18:49 +0300

    Form validation errors

--- examples/application/src/Form/Error.elm
+++ examples/application/src/Form/Error.elm
@@ -14,4 +14,27 @@ type Error
 
 toString : Error -> String
 toString error =
-    ""
+    case error of
+        IsEmpty ->
+            "This field is required"
+
+        NotAnInt ->
+            "This value must be an integer"
+
+        NotAValidEmail ->
+            "Not a valid email address"
+
+        TermsNotAccepted ->
+            "You must agree with the terms of service to complete the registration"
+
+        DoesNotMatchPassword ->
+            "Confirmation doesn’t match password"
+
+        PasswordTooShort ->
+            "The password must be at least eight characters long"
+
+        UsernameTaken ->
+            "This username is already taken"
+
+        NotAlphanumeric ->
+            "Only alphanumeric characters are allowed"   
